de907a4282bcf4f299799a14f3ab774c88b74796
Debashis Roy <goodquote39@hotmail.com>
2022-11-06 02:15:40 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Main from './Components/Main';
 import Home from './Components/Home/Home'
 import Products from './Components/Products/Products'
 import About from './Components/About/About'
+import Login from './Components/User/Login'
+import Register from './Components/User/Register'
 
 function App() {
   const router = createBrowserRouter([
@@ -27,6 +29,14 @@ function App() {
         {
           path: '/about',
           element: <About></About>
+        },
+        {
+          path:'/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
         }
       ]
     }
