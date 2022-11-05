1f45e9daefa0a5e2f28f222a001465b52818e2aa
Debashis Roy <goodquote39@hotmail.com>
2022-11-06 00:48:36 +0600

    basic router complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 
 import './App.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Main from './Components/Main';
+import Home from './Components/Home/Home'
+import Products from './Components/Products/Products'
+import About from './Components/About/About'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+        {
+          path: '/products',
+          element: <Products></Products>
+        },
+        {
+          path: '/about',
+          element: <About></About>
+        }
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <h1>Digital Hut</h1>
-    </div>
+    <RouterProvider router={router}></RouterProvider>
   );
 }
 
