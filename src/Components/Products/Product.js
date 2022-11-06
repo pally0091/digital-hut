a6d9bf6b0d3d21bba3a4549be071c99b07e8e5ff
Debashis Roy <goodquote39@hotmail.com>
2022-11-06 18:03:53 +0600

    firebase config added

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Product = ({product}) => {
     const {brand, description, price, thumbnail, title} =product;
     return (
-        <div className='border-2 mx-5 w-1/4 h-96'>
+        <div className='shadow-lg shadow-indigo-700 rounded p-2 mx-5 w-1/4 h-96'>
             <div className='p-1 w-full h-2/5'>
-                <img className='w-full h-full' src={thumbnail}></img>
+                <img className='w-full h-full' src={thumbnail} alt={title}></img>
             </div>
-            <h1>{title}</h1>
-            <h3>{brand}</h3>
-            <p>{description}</p>
-            <button>Buy Now : {price}</button>
+            <h1 className='text-lg font-bold text-black text-center'>{title}</h1>
+            <h3 className='text-center text-black border-2 rounded shadow-md mt-2'>{brand}</h3>
+            <p className='text-black text-justify mt-2 h-28'>{description}</p>
+            <button className='text-center w-full p-2 rounded bg-sky-400 hover:bg-green-400'>Buy Now : $ {price}</button>
         </div>
     );
 };
