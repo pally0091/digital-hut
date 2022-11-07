fbc90e436ad654f82746752b3efbcf596ab910fd
Debashis Roy <goodquote39@hotmail.com>
2022-11-07 07:24:34 +0600

    about section added

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const Product = ({product}) => {
-    const {brand, description, price, thumbnail, title} =product;
-    return (
-        <div className='shadow-lg shadow-indigo-700 rounded p-2 mx-5 w-1/4 h-96'>
-            <div className='p-1 w-full h-2/5'>
-                <img className='w-full h-full' src={thumbnail} alt={title}></img>
-            </div>
-            <h1 className='text-lg font-bold text-black text-center'>{title}</h1>
-            <h3 className='text-center text-black border-2 rounded shadow-md mt-2'>{brand}</h3>
-            <p className='text-black text-justify mt-2 h-28'>{description}</p>
-            <button className='text-center w-full p-2 rounded bg-sky-400 hover:bg-green-400'>Buy Now : $ {price}</button>
+const Product = ({ product }) => {
+  const { brand, description, price, thumbnail, title } = product;
+  return (
+    <div className="shadow-lg shadow-indigo-700 rounded p-2 mx-5 w-1/4 h-96">
+      <div className=" shadow-indigo-700 rounded   h-96">
+        <div className="p-1 w-full h-2/5">
+          <img
+            className="w-full h-full"
+            src={thumbnail}
+            alt={title}
+          ></img>
         </div>
-    );
+        <h1 className="text-lg font-bold text-black text-center">{title}</h1>
+        <h3 className="text-center text-black border-2 rounded shadow-md mt-2">
+          {brand}
+        </h3>
+        <p className="text-black text-justify mt-2 h-28">{description}</p>
+      </div>
+      <button className="text-center w-full p-2 rounded bg-sky-400 hover:bg-green-400">
+        Buy Now : $ {price}
+      </button>
+    </div>
+  );
 };
 
 export default Product;
