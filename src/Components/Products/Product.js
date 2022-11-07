e856dba58a0ab469a1b8dc0740d4532748cdebd0
Debashis Roy <goodquote39@hotmail.com>
2022-11-07 07:31:55 +0600

    card fixed

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -3,8 +3,8 @@ import React from "react";
 const Product = ({ product }) => {
   const { brand, description, price, thumbnail, title } = product;
   return (
-    <div className="shadow-lg shadow-indigo-700 rounded p-2 mx-5 w-1/4 h-96">
-      <div className=" shadow-indigo-700 rounded   h-96">
+    <div className="shadow-lg shadow-indigo-700 rounded p-2 mx-5 w-1/4 mt-5 border-2 border-black">
+      <div className=" rounded   h-96">
         <div className="p-1 w-full h-2/5">
           <img
             className="w-full h-full"
